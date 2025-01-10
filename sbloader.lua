84c4aa990d90f74aaefe31f7e7313a34b3cce933
cdkys <iloveadmist3@gmail.com>
2025-01-10 07:53:59 -0800

    Update sbloader.lua

--- sbloader.lua
+++ sbloader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7a82b6af1766034304f406505561fdd2.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0255d60abdbf7d781de06862294ad324.lua"))()
